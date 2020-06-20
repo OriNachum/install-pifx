6a7b57f79bc7a5736fb8123c87bb51887d561ddc
ori.nachum <ori.nachum@tipalti.com>
2020-06-21 01:32:10 +0300

    Added todo action in comment

--- pifx-installer.sh
+++ pifx-installer.sh
@@ -4,11 +4,16 @@ then {
     curl https://dotnet.microsoft.com/download/dotnet-core/scripts/v1/dotnet-install.sh > dotnet-install.sh
     chmod 777 dotnet-install.sh
     ./dotnet-install.sh
+
+    # TODO: Add this to .profile automatically
+    # # set PATH so it includes dotnet
+    # if [ -d "$HOME/.dotnet" ] ; then
+    #     PATH="$HOME/.dotnet:$PATH"
+    # fi
+
     echo "======================================================================================================"
     echo "================================ installed dotnet at /home/pi/.dotnet ================================"
     echo "======================================================================================================"
 }
 
 fi
-
-
