c726022f01c0ebc57a4e274df9e5fb7d4738ff0b
ori.nachum <ori.nachum@tipalti.com>
2020-06-21 18:28:27 +0300

    Added git clone and future plans

--- pifx-installer.sh
+++ pifx-installer.sh
@@ -17,3 +17,19 @@ then {
 }
 
 fi
+
+// Install Vue
+// Install RaspAP
+
+# if [ vue-lifx-server doesnt exist]
+git clone https://github.com/OriNachum/vue-lifx-server.git
+# fi
+
+cd ./LifxWebClient/LifxLanController
+cd ..
+
+# if [LifxWebClient doesn't exist]
+git clone https://github.com/OriNachum/LifxWebClient.git
+
+cd ./vue-lifx-server/hello-world
+cd ..
